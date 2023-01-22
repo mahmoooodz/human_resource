a06cd2419f0c2f7457a6bcbe088021ede92a9695
mahmoooodz <mahmoud.i.ahmad95@gmail.com>
2023-01-22 21:03:15 +0200

    Update employee.py

--- human_resource/human_resource/doctype/employee/employee.py
+++ human_resource/human_resource/doctype/employee/employee.py
@@ -2,7 +2,18 @@
 # For license information, please see license.txt
 
 # import frappe
+from frappe import *
 from frappe.model.document import Document
-
+from datetime import datetime
 class Employee(Document):
-	pass
+ def validate(doc): 
+  if doc.date_of_brith:
+    today = datetime.now()
+    dob = datetime.strptime(doc.date_of_brith, '%Y-%m-%d')
+    doc.age = today.year - dob.year - ((today.month, today.day) < (dob.month, dob.day))
+    if doc.age > 60:
+     throw("age cannot be more than 60 years.")
+		
+
+  else:
+	    throw("choose your birthday")
